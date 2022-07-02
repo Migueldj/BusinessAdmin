2a5a6df417b1899a61b8b9909d7464955e044936
Migueldj <36450896+Migueldj@users.noreply.github.com>
2022-07-02 01:23:30 -0500

    Adding fields to user and solving the problem with the password hash when updating

--- config/models.py
+++ config/models.py
@@ -1,37 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.contrib.auth.base_user import BaseUserManager
-from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
-
 # Create your models here.
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, password, **args):
         if not email:
-            raise ValueError(_('The Email must be set'))
+            raise ValueError('Email is a required field')
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, **args)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
+    def create_superuser(self, email, password, **args):
+        args.setdefault('is_staff', True)
+        args.setdefault('is_superuser', True)
+        args.setdefault('is_active', True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff=True.'))
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Superuser must have is_superuser=True.'))
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email, password, **args)
 
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_('email address'), unique=True)
+    email = models.EmailField('email address', unique=True)
+    name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -42,5 +36,9 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.email
 
     def save(self, *args, **kwargs):
-        self.set_password(self.password)
-        super(User, self).save(*args, **kwargs)
+        if self.pk is not None:
+            if User.objects.get(pk=self.pk).password != getattr(self, 'password'):
+                self.set_password(self.password)
+        else:
+            self.set_password(self.password)
+        return super(User, self).save(*args, **kwargs)
